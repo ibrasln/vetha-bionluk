4057df2d5eb211d924a413b25a9ad06c8a92c732
Ibrahim Aslan <i.aslan2020@gtu.edu.tr>
2024-05-30 00:25:40 +0300

    Bug fixing

--- BloomsTaxonomy/Assets/Scripts/UI/UIObject.cs
+++ BloomsTaxonomy/Assets/Scripts/UI/UIObject.cs
@@ -17,21 +17,11 @@ namespace UI
             if (OpenAnimation != null)
             {
                 OpenAnimation.OnStartEvent.AddListener(OnOpened);
-                Debug.Log(name + ": Added OnOpened listener to OpenAnimation");
-            }
-            else
-            {
-                Debug.LogWarning(name + ": OpenAnimation is null");
             }
 
             if (CloseAnimation != null)
             {
                 CloseAnimation.OnFinishedEvent.AddListener(OnClosed);
-                Debug.Log(name + ": Added OnClosed listener to CloseAnimation");
-            }
-            else
-            {
-                Debug.LogWarning(name + ": CloseAnimation is null");
             }
         }
 
@@ -51,14 +41,12 @@ namespace UI
 
         public void OnOpened()
         {
-            Debug.Log(name + " is opened!");
             IsOpened = true;
             IsClosed = false;
         }
 
         public void OnClosed()
         {
-            Debug.Log(name + " is closed!");
             IsClosed = true;
             IsOpened = false;
         }
